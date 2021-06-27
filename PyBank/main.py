c9bf06384e81158604abfa04053d9e8e8f5ff172
Kyle Shimberg <kshimberg@Kyles-MBP.attlocal.net>
2021-06-27 19:53:08 -0400

    Seventh Commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,40 +14,47 @@ with open(csvpath) as csvfile:
     # Skip header in data
     header = next(csvreader)
     
-  
+    # assign variables
     totalprofit = float(0)
     greatestincrease = float(0)
     greatestdecrease = float(0)
-    totalrows = int(0)
+    totalmonths = int(0)
+
     # Loop through data rows
     for row in csvreader:
-    
-        netchange = float(row[1])
+        
 
+        # netchange = float(row[1]), used for testing
+
+        # Add to total profit as each row progresses
         totalprofit = float(totalprofit) + float(row[1])
 
+        # Determine the Greatest Profit increase and assign value if greater than previous
         if float(row[1]) > float(greatestincrease):
             greatestincrease = float(row[1])
             increasemonth = row[0]
+
+        # Determine the Greatest Profit decreasr and assign value if greater than previous
         elif float(row[1]) < float(greatestdecrease):
             greatestdecrease = float(row[1])
             decreasemonth = row[0]
-    
-        totalrows += 1
 
-    averagechange = totalprofit/totalrows
+        # Count the number of months, assuming each row is new month
+        totalmonths += 1
+    # Calculate the average change
+    averagechange = totalprofit/totalmonths
+
+    #locale module used to format currency
     locale.setlocale(locale.LC_ALL, '')
+
+    # Print results
     print("Financial Analysis")
     print("-------------------")
-    print(f"Total Months:{totalrows}")
+    print(f"Total Months:{totalmonths}")
     print(f"Total: {locale.currency(totalprofit, grouping=True)}")
     print(f"Average Change: {locale.currency(averagechange, grouping=True)}")
-    print(f"Greatest Increase in Profits: {increasemonth} {locale.currency(greatestincrease, grouping=True)}")
-    print(f"Greatest Decrease in Profits: {decreasemonth} {locale.currency(greatestdecrease, grouping=True)}")
-
-
-
-
+    print(f"Greatest Increase in Profits: {increasemonth} ({locale.currency(greatestincrease, grouping=True)})")
+    print(f"Greatest Decrease in Profits: {decreasemonth} ({locale.currency(greatestdecrease, grouping=True)})")
 
 
 # Total Months: Count of data rows w/o header
